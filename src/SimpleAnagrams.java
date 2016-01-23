dcb05f9fc2e9750bd34ab5615add51c41d4576da
Alison <alison@johnston.name>
2016-01-23 10:01:52 +0000

    Added stringbuider and substring

--- src/SimpleAnagrams.java
+++ src/SimpleAnagrams.java
@@ -8,18 +8,54 @@ public class SimpleAnagrams {
 
     public static boolean checkUsingSorting(String word, String anagram){
 
-    	if(null == word || word.isEmpty() || null == anagram || anagram.isEmpty()){
-    		throw new IllegalArgumentException("Argument is null or empty.");
-    	}
-    	if(word.length() != anagram.length()){
-    		return false;
-    	}
+        if(word==null || word.isEmpty() || word == null || word.isEmpty()){
+            throw new IllegalArgumentException("word or anagram are null or empty");
+        }
 
-    	char[] charFromWord = word.toLowerCase().toCharArray();
-    	char[] charFromAnagram = anagram.toLowerCase().toCharArray();
-    	Arrays.sort(charFromWord);
-    	Arrays.sort(charFromAnagram);
+        char[] wordArray = word.toLowerCase().toCharArray();
+        char[] anagramArray = anagram.toLowerCase().toCharArray();
 
-    	return Arrays.equals(charFromWord,charFromAnagram);
+        Arrays.sort(wordArray);
+        Arrays.sort(anagramArray);
+
+        return Arrays.equals(wordArray, anagramArray);
+    }
+
+    public static boolean checkUsingSubstring(String word, String anagram){
+
+        if(word==null || word.isEmpty() || anagram == null || anagram.isEmpty()){
+            throw new IllegalArgumentException("word or anagram are null or empty");
+        }
+
+        char[] wordArray = word.toLowerCase().toCharArray();
+
+        for(char c : wordArray){
+            int i = anagram.toLowerCase().indexOf(c);
+            if(i == -1){
+                return false;
+            }
+            anagram = anagram.substring(0,i) + anagram.substring(i+1,anagram.length());
+        }
+
+        return anagram.length() == 0;
+    }
+
+    public static boolean checkUsingStringBuilder(String word, String anagram){        
+
+        if(word==null || word.isEmpty() || anagram == null || anagram.isEmpty()){
+            throw new IllegalArgumentException("word or anagram are null or empty");
+        }
+
+        char[] wordArray = word.toLowerCase().toCharArray();
+        StringBuilder result = new StringBuilder(anagram.toLowerCase());
+
+        for(char c : wordArray){
+            int i = result.indexOf(String.valueOf(c));
+            if( i == -1){
+                return false;
+            }
+            result.deleteCharAt(i);
+        }
+        return result.length() == 0;
     }
 }
--- tests/SimpleAnagramsTests.java
+++ tests/SimpleAnagramsTests.java
@@ -6,10 +6,11 @@ import static org.junit.Assert.assertTrue;
 public class SimpleAnagramsTests {
 
     @Test
-    public void testCheckAnagramUsingSorting(){
+    public void testCheckSimpleAnagramUsingSorting(){
 
         assertTrue(SimpleAnagrams.checkUsingSorting("word", "wrdo"));
         assertTrue(SimpleAnagrams.checkUsingSorting("mary", "army"));
+        assertTrue(SimpleAnagrams.checkUsingSorting("mary", "Army"));
         assertTrue(SimpleAnagrams.checkUsingSorting("stop", "tops"));
         assertTrue(SimpleAnagrams.checkUsingSorting("boat", "btoa"));
         assertFalse(SimpleAnagrams.checkUsingSorting("pure", "in"));
@@ -18,5 +19,103 @@ public class SimpleAnagramsTests {
         assertFalse(SimpleAnagrams.checkUsingSorting("ccc", "ccccccc"));
         assertTrue(SimpleAnagrams.checkUsingSorting("a", "a"));
         assertFalse(SimpleAnagrams.checkUsingSorting("sleep", "slep"));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSortingAnagramsNullWord(){
+        SimpleAnagrams.checkUsingSorting(null, "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleSortingAnagramsEmptyWord(){
+        SimpleAnagrams.checkUsingSorting("", "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleSortingAnagramsNullAnagram(){
+        SimpleAnagrams.checkUsingSorting(null, "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleSortingAnagramsEmptyAnagram(){
+        SimpleAnagrams.checkUsingSorting("", "wrdo");
+    }
+
+
+
+    @Test
+    public void testCheckSimpleAnagramUsingSubstring(){
+
+        assertTrue(SimpleAnagrams.checkUsingSubstring("word", "wrdo"));
+        assertTrue(SimpleAnagrams.checkUsingSubstring("mary", "army"));
+        assertTrue(SimpleAnagrams.checkUsingSubstring("mary", "Army"));
+        assertTrue(SimpleAnagrams.checkUsingSubstring("stop", "tops"));
+        assertTrue(SimpleAnagrams.checkUsingSubstring("boat", "btoa"));
+        assertFalse(SimpleAnagrams.checkUsingSubstring("pure", "in"));
+        assertFalse(SimpleAnagrams.checkUsingSubstring("fill", "fil"));
+        assertFalse(SimpleAnagrams.checkUsingSubstring("b", "bbb"));
+        assertFalse(SimpleAnagrams.checkUsingSubstring("ccc", "ccccccc"));
+        assertTrue(SimpleAnagrams.checkUsingSubstring("a", "a"));
+        assertFalse(SimpleAnagrams.checkUsingSubstring("sleep", "slep"));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSubstringAnagramsNullWord(){
+        SimpleAnagrams.checkUsingSubstring(null, "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleSubstringAnagramsEmptyWord(){
+        SimpleAnagrams.checkUsingSubstring("", "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleSubstringAnagramsNullAnagram(){
+        SimpleAnagrams.checkUsingSubstring(null, "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleSubstringAnagramsEmptyAnagram(){
+        SimpleAnagrams.checkUsingSubstring("", "wrdo");
+    }
+
+
+    @Test
+    public void testCheckSimpleAnagramUsingStringBuilder(){
+
+        assertTrue(SimpleAnagrams.checkUsingStringBuilder("word", "wrdo"));
+        assertTrue(SimpleAnagrams.checkUsingStringBuilder("mary", "army"));
+        assertTrue(SimpleAnagrams.checkUsingStringBuilder("mary", "Army"));
+        assertTrue(SimpleAnagrams.checkUsingStringBuilder("stop", "tops"));
+        assertTrue(SimpleAnagrams.checkUsingStringBuilder("boat", "btoa"));
+        assertFalse(SimpleAnagrams.checkUsingStringBuilder("pure", "in"));
+        assertFalse(SimpleAnagrams.checkUsingStringBuilder("fill", "fil"));
+        assertFalse(SimpleAnagrams.checkUsingStringBuilder("b", "bbb"));
+        assertFalse(SimpleAnagrams.checkUsingStringBuilder("ccc", "ccccccc"));
+        assertTrue(SimpleAnagrams.checkUsingStringBuilder("a", "a"));
+        assertFalse(SimpleAnagrams.checkUsingStringBuilder("sleep", "slep"));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleStringBuilderAnagramsNullWord(){
+        SimpleAnagrams.checkUsingStringBuilder(null, "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleStringBuilderAnagramsEmptyWord(){
+        SimpleAnagrams.checkUsingStringBuilder("", "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleStringBuilderAnagramsNullAnagram(){
+        SimpleAnagrams.checkUsingStringBuilder(null, "wrdo");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSimpleSimpleStringBuilderAnagramsEmptyAnagram(){
+        SimpleAnagrams.checkUsingStringBuilder("", "wrdo");
     }
 }
